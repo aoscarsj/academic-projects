831dcbd8bd08916c81a77ce989de98a53178b8f0
Artur Oscar Jr <arturoscar3@gmail.com>
2019-05-27 16:08:39 -0300

    Add novas funções

--- estDados/lista-A.c
+++ estDados/lista-A.c
@@ -10,15 +10,44 @@ cell_t *newCell();
 void insert(int, cell_t *);
 void delete(int, cell_t *);
 int /*cell_t * */search(int, cell_t *);
+int lookup(int, cell_t*);
+cell_t *concat(cell_t *, cell_t *);
 
 int main(){ // A - Lista com Encadeamento Simples
    cell_t *l = newCell();
+   cell_t *m = newCell();
    for(int i=0;i<10;i++) insert(i+1,l);
+   for(int i=10;i<20;i++) insert(i+1,m);
+   cell_t *n = concat(l,m);
+   while(n->next){
+      printf("%d ", n->next->element);
+      n= n->next;
+   }printf("\n");
+  // printf("%d\n", lookup(52,l));
   //delete(10, l);
    //printf("%d\n", l->next->element);
   // int t = search(10,l);
    //for(int i=-1;i<15;i++) printf("%d existe? %d\n", i, search(i,l));
    //printf("%d\n", t);
+   return 0;
+}
+cell_t *concat(cell_t *l, cell_t *m){
+   if(!l && !m) return NULL;
+   else if(!m || !m->next) return l;
+   else if (!l || !l->next) return m;
+   cell_t *p = l;
+   while(p->next)
+      p = p->next;
+   p->next = m->next;
+   free(m);
+   return l;
+} 
+int lookup(int x, cell_t *l){
+   if(!l) return 0;
+   while(l->next && l->next->element != x)
+      l = l->next;
+   if(l->next && l->next->element == x) 
+      return 1;
    return 0;
 }
 /*cell_t * */int search(int x, cell_t *l){
