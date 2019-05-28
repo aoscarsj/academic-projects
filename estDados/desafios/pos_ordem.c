0dbfe337dbdc20f810b3c4fe957a421c0f84b6a7
Your Name <you@example.com>
2019-05-28 13:58:32 -0300

    add

---
+++ estDados/desafios/pos_ordem.c
@@ -0,0 +1,8 @@
+#include <stdio.h>
+#include <stdlib.h>
+
+int main(){
+    /*
+        1- imprimir a árvore por nível de forma a preservar a visualização graficamente: um nível por linha, filhos esquerdo e direito de cada nó.
+    */
+}
