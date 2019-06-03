aef9dcb6e2cb1adb55f2953faf0714184ec9ad52
Artur Oscar Jr <arturoscar3@gmail.com>
2019-06-02 21:30:18 -0300

    Atualizações por Script

--- estDados/binary_tree.c
+++ estDados/binary_tree.c
@@ -1,21 +1,68 @@
 #include <stdio.h>
 #include <stdlib.h>
-
+#define MAX(a,b)((a)>(b)?(a):(b))
 typedef struct node node_t;
 struct node {
    int element;
    node_t *left, *right;
 };
-
-void print_in_order(node_t *);   // BFS   
-void print_pre_order(node_t *);  // BFS     
-void print_pos_order(node_t *);  // BFS     
+void print_BFS(node_t *); // BFS
+int height(node_t *);
+node_t *insert(int, node_t *);
+node_t *search(int, node_t *);
+node_t *newNode();
+void print_in_order(node_t *);   // DFS
+void print_pre_order(node_t *);  // DFS
+void print_pos_order(node_t *);  // DFS
 
 int main(){
-
+  node_t *tree;
+  for(int i=0;i<10;i++) tree = insert(i, tree);
+  print_pos_order(tree);
+  printf("\n" );
    return 0;
 }
+void print_BFS(node_t *t){
 
+}
+node_t *newNode(){
+  node_t *t = malloc(sizeof(node_t));
+  return t;
+}
+node_t *insert(int x, node_t *t){
+  if(!t){
+    t = newNode();
+    t->element = x;
+  }
+  else if(t->element > x)
+    t->left = insert(x,t->left);
+  else
+    t->right = insert(x, t->right);
+  return t;
+}
+node_t *search(int x, node_t *t){
+   //iterativa (Melhor)
+   while(t && t->element != x){
+     if(t->element > x)
+      t = t->left;
+     else
+      t = t->right;
+   }
+   return t;
+   /* //recursiva
+   if(t == NULL || t->element == x)
+     return t;
+   else if(t->element > x)
+     return search(x, t->left);
+   else
+     return search(x, t->right);
+
+  */
+}
+int height(node_t *t){
+  if(!t) return -1;
+  return (1+ MAX(height(t->left),height(t->right)));
+}
 void print_in_order(node_t *t){
    if(t){
       print_in_order(t->left);
@@ -27,7 +74,7 @@ void print_pre_order(node_t *t){
    if(t){
       printf("%d ", t->element);
       print_pre_order(t->left);
-      printf_pre_order(t->right);
+      print_pre_order(t->right);
    }
 }
 void print_pos_order(node_t *t){
@@ -37,5 +84,3 @@ void print_pos_order(node_t *t){
       printf("%d ", t->element);
    }
 }
-
-
