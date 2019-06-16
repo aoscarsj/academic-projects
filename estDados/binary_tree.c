b5cd53c09767c32276e1927f3da46cf5deff7653
Artur Oscar Jr <arturoscar3@gmail.com>
2019-06-16 19:57:44 -0300

    Atualizações por Script

--- estDados/binary_tree.c
+++ estDados/binary_tree.c
@@ -4,22 +4,29 @@
 #define debug printf("Foi\n");
 typedef struct node node_t;
 typedef enum {false, true}bool;
-typedef int element_t;
-typedef struct cell{
-  struct cell *next;
-  node_t *el;
-}cell_t;
+typedef node_t element_t;
 struct node {
    int element;
    node_t *left, *right;
 };
+typedef struct cell{
+  element_t *element;
+  struct cell *next;
+}cell_t;
 typedef struct queue{
-  cell_t *first, *last;
-  unsigned int size;
+    cell_t *first, *last;
+    unsigned int size, maxsize;
 }queue_t;
-bool enqueue(node_t *,queue_t *);
-bool dequeue(queue_t *);
+
+//queue
+cell_t *newCell();
+element_t *front(queue_t *);
+bool isEmpty(queue_t *);
+element_t *dequeue(queue_t *);
+bool enqueue(element_t *, queue_t *);
 queue_t *newQueue();
+//-------------------
+
 int height(node_t *);
 node_t *insert(int, node_t *);
 node_t *search(int, node_t *);
@@ -30,15 +37,15 @@ void DFS(node_t *, int);
 int main(){
   node_t *tree;
   //for(int i='a';i<'m';i++) tree = insert(i, tree);
-  tree = insert(97,tree); tree = insert(90,tree); tree = insert(100,tree); tree = insert(70,tree); tree = insert(95,tree); tree = insert(105,tree);
+  tree = insert(97,tree); tree = insert(90,tree); tree = insert(100,tree); tree = insert(70,tree); tree = insert(95,tree); tree = insert(99,tree);
   tree = insert(110,tree); tree = insert(60,tree);tree = insert(75,tree); tree =insert(92,tree);tree = insert(96,tree);
   //node_t *t = search(97, tree);
   //DFS(tree,1);
   printf("\n------------\n" );
   //DFS(tree,2);
-  //BFS(tree);
+  BFS(tree);
   printf("\n------------\n" );
-  //DFS(tree,3);
+//  DFS(tree,3);
   //printf("\n------------\n" );
   //printf("\n" );
   //printf("%c\n", tree->right->left->element);
@@ -107,41 +114,58 @@ void DFS(node_t *t, int opcao){
 
 
 void BFS(node_t *t){
-  node_t *aux = t;
+//  node_t *aux = t;
   queue_t *q = newQueue();
-  enqueue(aux, q);
+  enqueue(t, q);
   while(q->size>0){
-    debug;
-    if(aux->left)  enqueue(aux->left,q); //Erro ao entrar aqui
-    if(aux->right) enqueue(aux->right, q);
-    printf("%d ", q->first->el->element);
+    if(q->first->element->left)  enqueue(q->first->element->left,q); //Erro ao entrar aqui
+    if(q->first->element->right) enqueue(q->first->element->right, q);
+    printf("%d ", q->first->element->element);
     dequeue(q);
   }
+  printf("\n");
 }
-bool enqueue(node_t *x,queue_t *q){
-  if(!q) return false;
-  cell_t *cell;
-  if(q->size == 0) q->first = cell;
-  else q->last->next= cell;
-  q->last = cell;
-  cell->el = x;
-  q->size++;
-  return true;
-}
-bool dequeue(queue_t *q){
-  if(q->size == 0) return false;
-  cell_t *cell = q->first;
+element_t *dequeue(queue_t *q){
+  if(isEmpty(q)) return NULL;
+  cell_t *p = q->first;
   q->first = q->first->next;
-  if(q->last == cell) q->last=NULL;
-  free(cell);
+  if(q->last == p) q->last = NULL;
+  element_t *el = p->element;
+  free(p);
   q->size--;
-  return true;
+  return el;
 }
-queue_t *newQueue(){ // OK
+bool enqueue(element_t *x, queue_t *q){
+  cell_t *p = newCell();
+  if(!p) return false;
+  if(isEmpty(q)) q->first = p;
+  else q->last->next = p;
+  q->last = p;
+  (q->size)++;
+  p->element = x;
+  return true;
+}bool isEmpty(queue_t *q){
+  return (!q->last);
+}
+queue_t *newQueue(){
   queue_t *q = (queue_t*)malloc(sizeof(queue_t));
+
   if(q){
-    q-> first = q-> last = NULL;
     q->size = 0;
+    q ->first = q ->last = NULL;
   }
   return q;
+}
+cell_t *newCell(){
+  cell_t *q = (cell_t*)malloc(sizeof(cell_t));
+  if(q){
+    //*(q->element) = -1;
+    q->next = (cell_t*)NULL;
+    return q;
+  }
+  return (cell_t*)NULL;
+}
+element_t *front(queue_t *q){
+  if(isEmpty(q)) return NULL;
+  return q->first->element;
 }
