7492468f29187ea816a247576e02d463b3736c43
Artur Oscar Jr <arturoscar3@gmail.com>
2019-08-24 23:06:36 -0300

    Adicionando questao 3 PSB

---
+++ PSB/questao03.c
@@ -0,0 +1,14 @@
+//Multiplicacao de Matrizes n x n utilizando ponteiros
+
+#include <stdio.h>
+#include <stdlib.h>
+#define N 3
+int **alocarMatriz();
+int main(){
+
+	return 0;
+}
+int **alocarMatriz(){
+	int **p;
+	
+}
