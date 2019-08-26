faa10fcfa6b4566dc1fc0f104c388dfac59e73bb
Artur Oscar Jr <arturoscar3@gmail.com>
2019-08-26 18:21:44 -0300

    Adicionando multiplicacao de matrizes

--- PSB/questao03.c
+++ PSB/questao03.c
@@ -2,58 +2,57 @@
 
 #include <stdio.h>
 #include <stdlib.h>
-#define N 3
+
 //int **alocarmatrizA();
 int main(){
-	int **matrizA, **matrizB, i , j,k, resultado;
+	int **matrizA, **matrizB, **matrizC, i,j,k, resultado=0, N;
+
+	printf("Informe a quantidade de dimensoes das matrizes: ");
+	scanf("%d", &N);
 
 	matrizA = malloc (N * sizeof(int *));
 	matrizB = malloc (N * sizeof(int *));
-	if(matrizA == NULL || matrizB == NULL){
+	matrizC = malloc(N * sizeof(int *));
+
+	if(matrizA == NULL || matrizB == NULL || matrizC == NULL){
 		printf("Falha na alocacao de memoria!\n" );
 		return 0;
 	}
 	for(int i=0;i<N;i++){
 		*(matrizA+i) = malloc (N*sizeof(int));
 		*(matrizB+i) = malloc(N*sizeof(int));
+		*(matrizC+i) = malloc(N*sizeof(int));
 
-		if( *(matrizA+i)  == NULL || *(matrizB+i)  == NULL ){
+		if( *(matrizA+i)  == NULL || *(matrizB+i)  == NULL  || *(matrizC+i) == NULL){
 			printf("Falha na alocacao de memoria! Programa esta sendo encerrado!\n");
 			return 0;
 		}
 	}
+	printf("Informe os números da matriz A: ");
 	//lendo valores da matriz A
 	for(i=0;i<N;i++)
 		for(j=0;j<N;j++)
 			scanf("%d", &*(*(matrizA+i)+j));
-
+	printf("\nOK.\ninforme os números da matriz B: ");
 	//lendo valores da matriz B
 	for(i=0;i<N;i++)
 		for(j=0;j<N;j++)
 			scanf("%d", &*(*(matrizB+i)+j));
+
 	// pensar na logica da mult de matrizes.
-	for(k = 0;k<N;k++){
-		resultado = 0;
-		for(i=0;i<N;i++){
-			resultado += ( (*(*(matrizA+k)+i)) * (*( *(matrizB+i) +k)) );
-
+	for(i=0;i<N;i++){
+		for(j=0;j<N;j++){
+			for(k=0;k<N;k++){
+				*(*(matrizC+i)+j) += matrizA[i][k] * matrizB[k][j];
+			}
 		}
-		printf("%d\n", resultado);
 
 	}
-	for(int i =0 ;i<N;i++){
-		for(int j =0; j<N;j++){
-
-			printf("%d ",  *(*(matrizA+i)+j)   );
-		}
-		printf("\n" );
+	printf("\n\nResultado da Multiplicacao entre as matrizes. \n");
+	for(i=0;i<N;i++){
+		for(j=0;j<N;j++)printf("%d ", matrizC[i][j]);
+		printf("\n");
 	}
-	// int cont = 0;
-	// for(int i = 0 ; i< N; i ++){
-	// 	for(int j=0;j<N;j++){
-	// 		(*( (*(matrizA+i)) + j) )= cont++;
-	// 	}
-	// }
 	return 0;
 }
 
